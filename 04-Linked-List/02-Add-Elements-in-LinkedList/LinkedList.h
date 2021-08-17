f91981104add1f579367f32a2513ea622ae63fe3
azhong <zczwzw@163.com>
2021-08-18 00:05:13 +0800

    add element

--- 04-Linked-List/02-Add-Elements-in-LinkedList/LinkedList.h
+++ 04-Linked-List/02-Add-Elements-in-LinkedList/LinkedList.h
@@ -1,49 +1,34 @@
-//
-// Created by hpf on 18-5-8.
-//
-
-template<class T>
-class Node {
+#include <assert.h>
+template <typename T>
+class Node
+{
 public:
     T e;
     Node *next;
 
-    Node(T e, Node *next) : e(e), next(next) {
-    }
+    Node(T e, Node *next) : e(e), next(next) {}
 
-    Node(T e) : e(e), next(nullptr) {
-    }
+    Node(T e) : e(e), next(nullptr) {}
 
-    Node() : e(), next(nullptr){
-    }
+    Node() : e(), next(nullptr) {}
 };
 
-template<class T>
-class LinkedList {
+template <typename T>
+class LinkedList
+{
 private:
     Node<T> *head;
     int size;
+
 public:
-    class Range {
-    };
+    LinkedList() { head = nullptr; }
 
-    class Empty {
-    };
+    int getSize() { return size; }
 
-    LinkedList() {
-        head = nullptr;
-    }
+    bool isEmpty() { return size == 0; }
 
-    int getSize() {
-        return size;
-    }
-
-    bool isEmpty() {
-        return size == 0;
-    }
-
-
-    void addFirst(T e) {
+    void addFirst(T e)
+    {
         // Node<T> node = new Node<T>(e);
         // node.next = head;
         // head = node
@@ -51,8 +36,24 @@ public:
         size++;
     }
 
-    void addLast(T e) {
-        add(size, e);
+    void add(int index, T e)
+    {
+        assert(index < 0 || index > size);
+        if (index == 0)
+            addFirst(e);
+        else
+        {
+            Node<T> prev = head;
+            for (int i = 0; i < index; i++)
+                prev = prev.next;
+            // Node<T> node = new Node<T>(e);
+            // node.next = prev.next;
+            // prev.next = node;
+            prev.next = new Node<T>(e, prev.next);
+            size++;
+        }
     }
 
+    void addLast(T e) { add(size, e); }
+    
 };
