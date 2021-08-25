edfcaf52b1477632fa7ddbd66a869ae613023e0c
azhong <zczwzw@163.com>
2021-08-25 22:19:50 +0800

    add Element

---
+++ 06-Binary-Search-Tree/03-Add-Elements-in-BST/BST.h
@@ -0,0 +1,57 @@
+template <typename T>
+class Node
+{
+public:
+    T e;
+    Node<T> *left, right;
+    Node(T e) : e(e), left(nullptr), right(right);
+};
+
+template <typename T>
+class BST
+{
+public:
+    BST() : root(nullptr), size(0);
+
+    int size() { return size; }
+    bool isEmpty() { return size == 0; }
+
+    void add(T e)
+    {
+        if (root == nullptr)
+        {
+            root = new Node<T>(e);
+        }
+        else
+        {
+            add(root, e);
+        }
+    }
+
+private:
+    Node<T> *root;
+    int size;
+    void add(Node<T> *node, T e)
+    {
+        if (node->e == e)
+        {
+            return;
+        }
+        else if (node->e < e && node->left == nullptr)
+        {
+            node->left = new Node<T>(e);
+        }
+        else if (node->e > e && node->right == nullptr)
+        {
+            node->right = new Node<T>(e);
+        }
+        if (node->e < e)
+        {
+            add(node->left, e);
+        }
+        else
+        {
+            add(node->right, e);
+        }
+    }
+};
