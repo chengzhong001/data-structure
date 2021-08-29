b42fff65833c6d8a42c74be0343bdf8321130f84
azhong <zczwzw@163.com>
2021-08-29 18:57:52 +0800

    bst remove a specified element

--- 06-Binary-Search-Tree/12-Remove-Elements-in-BST/main.cpp
+++ 06-Binary-Search-Tree/12-Remove-Elements-in-BST/main.cpp
@@ -12,21 +12,19 @@ int main(int argc, char const *argv[])
     const unsigned int n = 10;;
     for (int i = 0; i < n; i++)
         bst->add(u(e));
-    bst->inOrder();
+
+    bst->preOrder();
+    bst->remove(66);
+
     std::vector<int> nums;
   
-
-    // while (!bst->isEmpty())
-    //     nums.push_back(bst->removeMin());
-    // for(int num: nums)
-    //     std::cout << num << " ";
-    // std::cout << std::endl;;
-
     while (!bst->isEmpty())
-        nums.push_back(bst->removeMax());
+        nums.push_back(bst->removeMin());
     for(int num: nums)
         std::cout << num << " ";
     std::cout << std::endl;;
+
+  
     
     return 0;
 }
