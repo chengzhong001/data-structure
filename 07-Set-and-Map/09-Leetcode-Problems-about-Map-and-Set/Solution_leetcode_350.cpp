93fed75be279564e885dc9884929d381c5c55445
azhong <zczwzw@163.com>
2021-09-03 22:10:08 +0800

    commit leetcode 350

--- 07-Set-and-Map/09-Leetcode-Problems-about-Map-and-Set/Solution_leetcode_350.cpp
+++ 07-Set-and-Map/09-Leetcode-Problems-about-Map-and-Set/Solution_leetcode_350.cpp
@@ -1,6 +1,7 @@
 #include <vector>
-#include <set>
+#include <map>
 #include <iostream>
+#include <vector>
 using namespace std;
 
 class Solution
@@ -8,21 +9,30 @@ class Solution
 public:
     vector<int> intersection(vector<int> &nums1, vector<int> &nums2)
     {
-
-        // vector<int> res;
-        set<int> numset1;
-        set<int> numset2;
+        vector<int> res;
+        map<int, int> num_map1;
         for (int num : nums1)
         {
-            numset1.insert(num);
+            if (!num_map1.count(num))
+            {
+                num_map1[num] = 1;
+            }
+            else
+            {
+                num_map1[num] = num_map1[num] + 1;
+            }
         }
-
-        for(int num: nums2){
-            if(numset1.count(num))
-                numset2.insert(num);
+        for (int num : nums2)
+        {
+            if (num_map1.count(num))
+            {
+                res.push_back(num);
+                num_map1[num] -= 1;
+                if(num_map1[num]== 0){
+                    num_map1.erase(num);
+                }
+            }
         }
-
-        vector<int> res(numset2.begin(),numset2.end());
         return res;
     }
 };
@@ -32,12 +42,13 @@ int main(int argc, char const *argv[])
     /* code */
     vector<int> nums1 = {1, 2, 2, 3, 1};
     vector<int> nums2 = {2, 2};
+
+
     vector<int> res = Solution().intersection(nums1, nums2);
 
     for(int num: res){
         std::cout << num << "\n";
     }
-    
 
     return 0;
 }
