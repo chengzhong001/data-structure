48dec05203c78fb2edf4370c5c3315708b768aec
azhong <zczwzw@163.com>
2021-09-17 23:54:08 +0800

    format

--- 09-Segment-Tree/05-Segment-Tree-Problems-in-Leetcode/NumArray.cpp
+++ 09-Segment-Tree/05-Segment-Tree-Problems-in-Leetcode/NumArray.cpp
@@ -2,20 +2,24 @@
 using namespace std;
 
 // leetcode 303
-class NumArray {
+class NumArray
+{
 public:
-    NumArray(vector<int> nums) {
-    	sum = new int[nums.size() + 1];
-    	sum[0] = 0;
-    	for(int i = 1; i < nums.size() + 1; i++) {
-    		sum[i] = sum[i - 1] + nums[i - 1];
-    	}
-    }
+	NumArray(vector<int> nums)
+	{
+		sum = new int[nums.size() + 1];
+		sum[0] = 0;
+		for (int i = 1; i < nums.size() + 1; i++)
+		{
+			sum[i] = sum[i - 1] + nums[i - 1];
+		}
+	}
 
-    int sumRange(int i, int j) {
-    	return sum[j + 1] - sum[i];
-    }
-    
+	int sumRange(int i, int j)
+	{
+		return sum[j + 1] - sum[i];
+	}
+
 private:
 	int *sum;
 };
