b8f77ae4fcdeab98ab3e704d98361223c1f4ea8d
azhong <zczwzw@163.com>
2021-11-18 23:26:20 +0800

    update

--- 10-Trie/02-Trie-Basics/main.cpp
+++ 10-Trie/02-Trie-Basics/main.cpp
@@ -15,13 +15,9 @@ int main(int argc, char const *argv[])
 
         BSTSet<std::string> bstset;
         for (std::string word : words)
-        {
             bstset.add(word);
-        }
         for (std::string word : words)
-        {
             bstset.contains(word);
-        }
         clock_t endTime = clock();
         double time = (double)(endTime - startTime) / CLOCKS_PER_SEC;
         std::cout << "BST Time: " << time << std::endl;
@@ -30,13 +26,10 @@ int main(int argc, char const *argv[])
 
         Trie trie;
         for (std::string word : words)
-        {
             trie.add(word);
-        }
+    
         for (std::string word : words)
-        {
             trie.contains(word);
-        }
         endTime = clock();
         time = (double)(endTime - startTime) / CLOCKS_PER_SEC;
         std::cout << "Trie Time: " << time << std::endl;
