d4680c313b27b38ae5f085e7b08fa01154f27901
azhong <zczwzw@163.com>
2021-11-20 23:57:51 +0800

    search in trie

--- 10-Trie/02-Trie-Basics/main.cpp
+++ 10-Trie/02-Trie-Basics/main.cpp
@@ -1,37 +1,7 @@
 #include <iostream>
-#include <map>
-#include <vector>
-#include "FileOperation.h"
-#include "BSTSet.h"
 #include "Trie.h"
 
 int main(int argc, char const *argv[])
 {
-    std::vector<std::string> words;
 
-    if (FileOps::readFile("pride-and-prejudice.txt", words))
-    {
-        clock_t startTime = clock();
-
-        BSTSet<std::string> bstset;
-        for (std::string word : words)
-            bstset.add(word);
-        for (std::string word : words)
-            bstset.contains(word);
-        clock_t endTime = clock();
-        double time = (double)(endTime - startTime) / CLOCKS_PER_SEC;
-        std::cout << "BST Time: " << time << std::endl;
-
-        startTime = clock();
-
-        Trie trie;
-        for (std::string word : words)
-            trie.add(word);
-    
-        for (std::string word : words)
-            trie.contains(word);
-        endTime = clock();
-        time = (double)(endTime - startTime) / CLOCKS_PER_SEC;
-        std::cout << "Trie Time: " << time << std::endl;
-    }
 }
--- 10-Trie/03-Searching-in-Trie/Trie.h
+++ 10-Trie/03-Searching-in-Trie/Trie.h
@@ -38,4 +38,14 @@ public:
         
     }
 
+    bool contains(std::string word){
+        Node *cur = root;
+        for(char c: word){
+            if(cur->next.find(c) == cur->next.end())
+                return false;
+            cur = &cur->next.find(c)->second;
+        }
+        return cur->isWord;
+    }
+
 };
