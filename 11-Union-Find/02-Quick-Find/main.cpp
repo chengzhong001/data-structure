b9f8a83a84bba065434b53cf0c1c2db680c7ec17
azhong <zczwzw@163.com>
2021-12-06 22:52:25 +0800

    quick find

---
+++ 11-Union-Find/02-Quick-Find/main.cpp
@@ -0,0 +1,14 @@
+#include "UnionFind.h"
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    int a[] = {1, 2, 3, 4, 8};
+    for (auto i : a)
+    {
+        std::cout << i << " ";
+    }
+
+    return 0;
+}
---
+++ 11-Union-Find/03-Quick-Union/UnionFind.h
@@ -0,0 +1,43 @@
+#include "UF.h"
+
+class UnionFind1 : public UF
+{
+public:
+    UnionFind1(int capacty)
+    {
+        id = new int[capacty];
+        size = capacty;
+        for (int i = 0; i < size; i++)
+            id[i] = i;
+    }
+    int getSize() override { return size; }
+    bool isConnected(int p, int q) override { return find(p) == find(q); }
+    void unionElements(int p, int q) override
+    {
+        int pID = find(p);
+        int qID = find(q);
+        if (pID == qID)
+            return;
+        for (int i = 0; i < size; i++)
+        {
+            if (id[i] = pID)
+                id[i] = qID;
+        }
+    }
+
+    ~UnionFind1()
+    {
+        delete[] id;
+    }
+
+private:
+    int *id;
+    int size;
+
+    int find(int p)
+    {
+        if (p < 0 || p >= size)
+            throw std::runtime_error("p is out of bound");
+        return id[p];
+    }
+};
