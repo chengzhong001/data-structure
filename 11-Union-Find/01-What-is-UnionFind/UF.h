00722ea63401055f82571d4cb63f1e2a5df280ea
azhong <zczwzw@163.com>
2021-12-06 21:59:30 +0800

    uf

---
+++ 11-Union-Find/01-What-is-UnionFind/UF.h
@@ -0,0 +1,7 @@
+class UF
+{
+public:
+    virtual int getSize();
+    virtual bool isConnected(int p, int q);
+    virtual void unionElements(int p, int q);
+};
---
+++ 11-Union-Find/02-Quick-Find/UF.h
@@ -0,0 +1,8 @@
+class UF
+{
+public:
+    virtual int getSize();
+    virtual bool isConnected(int p, int q);
+    virtual void unionElements(int p, int q);
+};
+
